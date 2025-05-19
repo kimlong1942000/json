538a11cbb45bc42c220d9cdd9bb555766f24605b
kimlong1942000 <nguyenkimlong1942000@gmail.com>
2025-05-19 14:23:45 +0700

    2nd commit

--- jsondump2.cpp
+++ jsondump2.cpp
@@ -1,75 +1,80 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <algorithm>
+#include <map>
+#include <algorithm> // for remove_if
 
 class JsonX {
 private:
-    std::string jsonStr;
+    std::map<std::string, std::string> _data;
 
 public:
-    // Constructor để khởi tạo từ chuỗi JSON
-    JsonX(const std::string& jsonString) : jsonStr(jsonString) {}
+    JsonX(const std::string& jsonStr) {
+        parseJson(jsonStr);
+    }
 
-    // Phương thức lấy giá trị của key
-    std::string operator[](const std::string& key) {
-        return getValueForKey(jsonStr, key);
+    std::string operator[](const std::string& key) const {
+        auto it = _data.find(key);
+        if (it != _data.end()) {
+            return it->second;
+        }
+        return ""; // Không tìm thấy
     }
 
 private:
-    // Hàm tách và lấy giá trị dựa trên key
-    std::string getValueForKey(const std::string& jsonStr, const std::string& key) {
-        std::string searchKey = "\"" + key + "\"";
-        size_t keyPos = jsonStr.find(searchKey);
-        if (keyPos == std::string::npos) {
-            return ""; // Không tìm thấy key
-        }
-        size_t colonPos = jsonStr.find(":", keyPos);
-        if (colonPos == std::string::npos) {
-            return "";
-        }
-        size_t valueStart = colonPos + 1;
+    void parseJson(const std::string& jsonStr) {
+        // Loại bỏ khoảng trắng để dễ phân tích
+        std::string s = jsonStr;
+        s.erase(std::remove_if(s.begin(), s.end(), ::isspace), s.end());
 
-        while (valueStart < jsonStr.size() && isspace(jsonStr[valueStart])) {
-            valueStart++;
-        }
+        // Giả sử JSON dạng đơn giản: { "key": "value", "key2": "value2" }
+        size_t pos = 0;
+        while ((pos = s.find("\"", pos)) != std::string::npos) {
+            size_t keyStart = pos + 1;
+            size_t keyEnd = s.find("\"", keyStart);
+            if (keyEnd == std::string::npos) break;
+            std::string key = s.substr(keyStart, keyEnd - keyStart);
 
-        if (jsonStr[valueStart] == '\"') {
-            size_t valueEnd = jsonStr.find('\"', valueStart + 1);
-            if (valueEnd == std::string::npos) return "";
-            return jsonStr.substr(valueStart + 1, valueEnd - valueStart - 1);
-        } else {
-            size_t valueEnd = valueStart;
-            while (valueEnd < jsonStr.size() &&
-                jsonStr[valueEnd] != ',' && jsonStr[valueEnd] != '}' && !isspace(jsonStr[valueEnd])) {
-                valueEnd++;
+            size_t colonPos = s.find(":", keyEnd);
+            if (colonPos == std::string::npos) break;
+            size_t valueStart = colonPos + 1;
+
+            // Tìm giá trị
+            if (s[valueStart] == '\"') {
+                size_t valueEnd = s.find("\"", valueStart + 1);
+                if (valueEnd == std::string::npos) break;
+                std::string value = s.substr(valueStart + 1, valueEnd - valueStart - 1);
+                _data[key] = value;
+                pos = valueEnd + 1;
+            } else {
+                // Trường hợp giá trị không phải chuỗi
+                size_t valueEnd = valueStart;
+                while (valueEnd < s.size() && s[valueEnd] != ',' && s[valueEnd] != '}') {
+                    valueEnd++;
+                }
+                std::string value = s.substr(valueStart, valueEnd - valueStart);
+                _data[key] = value;
+                pos = valueEnd;
             }
-            std::string val = jsonStr.substr(valueStart, valueEnd - valueStart);
-            val.erase(std::remove_if(val.begin(), val.end(), ::isspace), val.end());
-            return val;
         }
     }
 };
 
 int main() {
-    // Đọc dữ liệu JSON từ file
+    // Đọc file JSON
     std::ifstream file("data.txt");
     if (!file.is_open()) {
         std::cerr << "Không thể mở file." << std::endl;
         return 1;
     }
     std::string jsonStr((std::istreambuf_iterator<char>(file)),
-        std::istreambuf_iterator<char>());
-
-    jsonStr.erase(std::remove_if(jsonStr.begin(), jsonStr.end(), ::isspace), jsonStr.end());
+                        std::istreambuf_iterator<char>());
 
     JsonX json(jsonStr);
-    std::string name = json["name"];
-    std::cout << "name: " << name << std::endl;
-    std::string age = json["age"];
-    std::cout << "age: " << age << std::endl;
-    std::string addressStr = json["address"]; 
 
+    std::cout << "name: " << json["name"] << std::endl;
+    std::cout << "age: " << json["age"] << std::endl;
+    std::cout << "city: " << json["city"] << std::endl;
 
     return 0;
 }
